42478fc29cd35d28856a5bf47d3e43fe5cd0ac56
Matei Tudose <princematthew_secure@protonmail.com>
2020-01-24 19:39:50 +0200

    Update improved_address_verifier.js
    Automatization of the program, but with a few bugs, like the web browser page not closing on driver.quit();

--- improved_address_verifier.js
+++ improved_address_verifier.js
@@ -14,55 +14,75 @@ var driver = new webdriver.Builder()
 
 const bitcore = require('bitcore-lib');
 
-var privateKey = new bitcore.PrivateKey();
+function loop() {
 
-var address = privateKey.toAddress();
+    var privateKey = new bitcore.PrivateKey();
 
-console.log("This is the private key: " + privateKey);
+    var address = privateKey.toAddress();
 
-console.log("This is the public address: " + address);
+    console.log("This is the private key: " + privateKey);
 
-function check_balance() {
-    return new Promise((resolve, reject) => {
+    console.log("This is the public address: " + address);
 
-        driver.get("https://explorer.bitcoin.com/btc/address/" + address);
+    function check_balance() {
+        return new Promise((resolve, reject) => {
 
-        let balance_amount = driver.findElement(webdriver.By.className("amount")).getText();
+            driver.get("https://explorer.bitcoin.com/btc/address/" + address);
 
-        if (balance_amount > 0) {
-            resolve('This wallet has ' + balance_amount + ' Bitcoins.');
-        }
-        else {
-            reject('0 Bitcoins!');
-        }
+            let balance_amount = driver.findElement(webdriver.By.className("amount")).getText();
 
-    });
+            if (balance_amount > 0) {
+                resolve('This wallet has ' + balance_amount + ' Bitcoins.');
+            }
+            else {
+                reject('0 Bitcoins!');
+            }
 
+        });
+
+    }
+
+    check_balance().then((message) => {
+
+        console.log(message);
+
+    }).catch((message) => {
+
+        console.log(message);
+
+    })
+
+    function finish_process() {
+        return new Promise((resolve, reject) => {
+            setTimeout(function () {
+                console.log('Closed tab successfully!');
+                resolve();
+            }, 8000);
+        }).catch((error) => {
+            console.log('Unexpected error occured: ' + error);
+        })
+    }
+
+    let restart = finish_process().then(function () {
+        setTimeout(function () {
+            console.log('Reinitializing process...')
+        }, 3000);
+
+    })
 }
 
-check_balance().then((message) => {
+var number = 0;
+function twentySeconds() {
+    if (number <= 10) {
+        loop();
+        number++;
+        console.log('Try number: ' + number);
+    }
+};
 
-    console.log(message);
+if (number > 10) {
+    clearInterval(twentySeconds);
+}
 
-}).catch((message) => {
+setInterval(twentySeconds, 20000);
 
-    console.log(message);
-
-})
-
-let finish_process = new Promise((resolve, reject) => {
-    setTimeout(function () {
-        console.log('Closed tab successfully!');
-        resolve();
-        driver.quit();
-    }, 8000);
-}).catch((error) => {
-console.log('Unexpected error occured: ' + error);
-})
-
-finish_process.then(function () {
-    setTimeout(function () {
-        console.log('Reinitializing process...')
-    }, 3000);
-
-})
